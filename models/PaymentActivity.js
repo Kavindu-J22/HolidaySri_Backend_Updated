2173e985bce2f65c81e1de5b1f71d1991c472430
Kavindu Jayasinghe <87432677+Kavindu-J22@users.noreply.github.com>
2025-08-04 04:38:05 +0530

    up

--- models/PaymentActivity.js
+++ models/PaymentActivity.js
@@ -22,7 +22,7 @@ const paymentActivitySchema = new mongoose.Schema({
   category: {
     type: String,
     required: true,
-    enum: ['Promo Codes', 'Membership', 'HSC Purchase', 'Advertisement', 'Advertisement Fee - Selling Promocode', 'Commercial Partnership', 'Access Fee'],
+    enum: ['Promo Codes', 'Membership', 'HSC Purchase', 'Advertisement', 'Advertisement Fee - Selling Promocode', 'Commercial Partnership', 'Access Fee', 'Promo Code Renewal'],
     default: 'Promo Codes'
   },
   originalAmount: {
