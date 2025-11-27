2dda0fdb4426fc63e3705ee5d84f7e65c639abac
Kavindu-J22 <kavindujayasinghe2002122@gmail.com>
2025-11-27 20:37:46 +0530

    up

--- models/RentLandCampingParking.js
+++ models/RentLandCampingParking.js
@@ -160,6 +160,36 @@ const rentLandCampingParkingSchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
+  // Reviews array
+  reviews: [{
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    userName: {
+      type: String,
+      required: true
+    },
+    userImage: {
+      type: String,
+      default: ''
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
+    },
+    reviewText: {
+      type: String,
+      default: ''
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }],
   averageRating: {
     type: Number,
     default: 0,
